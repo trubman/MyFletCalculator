045cbf45d3eb3b93d3dc9b22e75dc6a110691e2d
trubman <trub.ma.an@gmail.com>
2024-10-12 20:50:37 +0300

    Переделал test_calc и calc

--- calc.py
+++ calc.py
@@ -1,60 +1,40 @@
+from referent import Referent
 
 
 class Calculator:
 
-    def __init__(self):
-        self._num1 = 0
-        self._num2 = 0
+    def add(self, num1, num2):
+        result = num1 + num2
+        return result
 
-    def set_value(self, num1, num2):
+    def substract(self, num1, num2):
+        result = num1 - num2
+        return result
+
+    def multiply(self, num1, num2):
+        result = num1 * num2
+        return result
+
+    def divide(self, num1, num2):
         try:
-            self._num1 = int(num1)
-            self._num2 = int(num2)
-        except ValueError:
-            print("Допустимы только целые числа!")
-
-    def get_value(self):
-        print(f'Текущия значения: {self._num1} и {self._num2}')
-
-    def add(self):
-        result = self._num1 + self._num2
-        print(f'{self._num1} + {self._num2} = {result}')
-        return result
-
-    def substract(self):
-        result = self._num1 - self._num2
-        print(f'{self._num1} - {self._num2} = {result}')
-        return result
-
-    def multiply(self):
-        result = self._num1 * self._num2
-        print(f'{self._num1} * {self._num2} = {result}')
-        return result
-
-    def divide(self):
-        try:
-            result = self._num1 / self._num2
-            print(f'{self._num1} / {self._num2} = {result:.2f}')
+            result = num1 / num2
             return result
         except ZeroDivisionError:
-            print("На ноль делить нельзя! (Во всяком случае в этом калькуляторе)")
+            Referent.do_output("На ноль делить нельзя! (Во всяком случае в этом калькуляторе)")
+            return False
 
-
-class AdvancedCalculator(Calculator):
-
-    def power(self):
-        result = self._num1 ** self._num2
-        print(f'{self._num1} ** {self._num2} = {result}')
+    def power(self, num1, num2):
+        result = num1 ** num2
         return result
 
-    def square_root(self):
+    def square_root(self, num1, num2):
         try:
-            if self._num1 >= 0:
-                result = self._num1 ** (1/self._num2)
-                print(f'{self._num2}√{self._num1} = {result}')
+            if num1 >= 0:
+                result = num1 ** (1/num2)
                 return result
             else:
                 raise ValueError
         except ValueError:
-            print("Брать корень из отрицательного числа мы Вам не позволим!")
+            Referent.do_output("Брать корень из отрицательного числа мы Вам не позволим!")
+            return False
 
--- test_calc.py
+++ test_calc.py
@@ -1,91 +1,43 @@
 import pytest
-from calc import Calculator, AdvancedCalculator
+from calc import Calculator
 
 
 class TestCalculators:
 
-    @pytest.fixture(scope="class")
-    def prepare_calculators(self):
-        Calc = Calculator()
-        AdvCalc = AdvancedCalculator()
-        return {'Calc': Calc, 'AdvCalc': AdvCalc}
-
     @pytest.mark.parametrize(
-        'name,value,expectations',
+        'operation,num1,num2,expectations',
         [
-            pytest.param('Calc', ('a', 'b'), "Допустимы только целые числа!\n",
-                         id="try input 'a', 'b' in Calc"),
-            pytest.param('AdvCalc', ('a', 'b'), "Допустимы только целые числа!\n",
-                         id="try input 'a', 'b' in AdvCalc"),
+            pytest.param('add', 2, 2, 4, id='add'),
+            pytest.param('substract', 2, 2, 0, id='substract'),
+            pytest.param('multiply', 2, 2, 4, id='multiply'),
+            pytest.param('divide', 2, 2, 1, id='divide'),
+            pytest.param('power', 2, 3, 8, id='power'),
+            pytest.param('square_root', 25, 2, 5, id='square_root'),
         ]
     )
-    def test_set_and_get_value_error_works(self, name, value, expectations, prepare_calculators, capsys):
-        """ Проверим функцию set_value() с недопустимыми входными значениями"""
-        # GIVEN/ДАНО prepare_calculators, name, value, expectations
-        # WHEN/КОГДА
-        prepare_calculators[name].set_value(*value)
-        captured = capsys.readouterr()
-        # THEN/ТОГДА
-        assert captured.out == expectations
+    def test_standart_operations(self, operation, num1, num2, expectations):
+        calc = Calculator()
+
+        result = calc.__getattribute__(operation).__call__(num1, num2)
+
+        assert result == expectations
 
     @pytest.mark.parametrize(
-        'name,value,expectations',
+        'operation,num1,num2,expectations',
         [
-            pytest.param('Calc', (5, 6), "Текущия значения: 5 и 6\n",
-                         id="input 5, 6 in Calc"),
-            pytest.param('AdvCalc', (5, 6), "Текущия значения: 5 и 6\n",
-                         id="input 5, 6 in AdvCalc"),
+            pytest.param('divide', 2, 0,
+                         "На ноль делить нельзя! (Во всяком случае в этом калькуляторе)\n",
+                         id='divide'),
+            pytest.param('square_root', -25, 2,
+                         "Брать корень из отрицательного числа мы Вам не позволим!\n",
+                         id='square_root'),
         ]
     )
-    def test_set_and_get_value_normal_works(self, name, value, expectations, prepare_calculators, capsys):
-        """ Проверим функцию set_value() """
-        # GIVEN/ДАНО prepare_calculators, name, value, expectations
-        # WHEN/КОГДА
-        prepare_calculators[name].set_value(*value)
-        prepare_calculators[name].get_value()
-        captured = capsys.readouterr()
-        # THEN/ТОГДА
-        assert captured.out == expectations
+    def test_standart_operations_error(self, operation, num1, num2, expectations, capsys):
+        calc = Calculator()
 
-    @pytest.mark.parametrize(
-        'name,operation,expectations',
-        [
-            pytest.param('Calc', 'add', "5 + 6 = 11\n", id="5 + 6 in Calc"),
-            pytest.param('AdvCalc', 'add', "5 + 6 = 11\n", id="5 + 6 in AdvCalc"),
-            pytest.param('Calc', 'substract', "5 - 6 = -1\n", id="5 - 6 in Calc"),
-            pytest.param('AdvCalc', 'substract', "5 - 6 = -1\n", id="5 - 6 in AdvCalc"),
-            pytest.param('Calc', 'multiply', "5 * 6 = 30\n", id="5 * 6 in Calc"),
-            pytest.param('AdvCalc', 'multiply', "5 * 6 = 30\n", id="5 * 6 in AdvCalc"),
-            pytest.param('Calc', 'divide', "5 / 6 = 0.83\n", id="5 / 6 in Calc"),
-            pytest.param('AdvCalc', 'divide', "5 / 6 = 0.83\n", id="5 / 6 in AdvCalc"),
-        ]
-    )
-    def test_standart_operations(self, name, operation, expectations, prepare_calculators, capsys):
-        """ Проверим выполнение операций в Calc (и у его нследника AdvCalc) """
-        # GIVEN/ДАНО name, operation, expectations, prepare_calculators
-        # WHEN/КОГДА
-        prepare_calculators[name].__getattribute__(operation).__call__()
-        captured = capsys.readouterr()
-        # THEN/ТОГДА
-        assert captured.out == expectations
+        result = calc.__getattribute__(operation).__call__(num1, num2)
+        result_print = capsys.readouterr().out
 
-    @pytest.fixture(scope='class')
-    def prepare_to_test_advanced_operations(self, prepare_calculators):
-        prepare_calculators['AdvCalc'].set_value(25, 2)
-        return prepare_calculators
+        assert result_print == expectations
 
-    @pytest.mark.parametrize(
-        'operation,expectations',
-        [
-            pytest.param('power', "25 ** 2 = 625\n", id="25 ** 2 in AdvCalc"),
-            pytest.param('square_root', "2√25 = 5.0\n", id="square_root25 in AdvCalc"),
-        ]
-    )
-    def test_advanced_operations(self, operation, expectations, prepare_to_test_advanced_operations, capsys):
-        """ Проверим выполнение продвинутых операций в AdvCalc """
-        # GIVEN/ДАНО operation, expectations, prepare_calculators
-        # WHEN/КОГДА
-        prepare_to_test_advanced_operations['AdvCalc'].__getattribute__(operation).__call__()
-        captured = capsys.readouterr()
-        # THEN/ТОГДА
-        assert captured.out == expectations
