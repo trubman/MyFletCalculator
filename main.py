2403f41c38d4c3d1b0da62e0bafb314619eed742
trubman <trub.ma.an@gmail.com>
2024-10-12 16:28:28 +0300

    Create main.py, test_referent.py
    Создал "точку входа" для приложения, задумался о функциональном тесте

---
+++ main.py
@@ -0,0 +1,14 @@
+import flet as ft
+
+# CLI or FLET
+MODE = 'CLI'
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    if MODE == 'CLI':
+        pass
+    elif MODE == 'FLET':
+        ft.app(main)
+
